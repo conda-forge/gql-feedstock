9f67aade3ac473354ff37805197308889c476478
Leszek Hanusz <leszek.hanusz@gmail.com>
2025-02-19 17:12:08 +0100

    Update recipe same as PR #24
    https://github.com/conda-forge/gql-feedstock/pull/24

---
+++ recipe/run_gql_with_all.py
@@ -0,0 +1,24 @@
+import sys
+from subprocess import call
+
+FAIL_UNDER = "95"
+COV = ["coverage"]
+RUN = ["run", "--source=gql", "--branch", "-m"]
+PYTEST = ["pytest", "-vv", "--color=yes", "--tb=long"]
+REPORT = ["report", "--show-missing", "--skip-covered", f"--fail-under={FAIL_UNDER}"]
+
+SKIPS = [
+]
+
+
+SKIP_OR = " or ".join(SKIPS)
+K = ["-k", f"not ({SKIP_OR})"]
+
+
+if __name__ == "__main__":
+    sys.exit(
+        # run the tests
+        call([*COV, *RUN, *PYTEST, *K])
+        # maybe run coverage
+        or call([*COV, *REPORT])
+    )
